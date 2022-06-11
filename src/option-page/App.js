ca1e47cc07dff61f977ed5c7f4af52765ab78549
mocraimer <good2k3@gmail.com>
2022-06-11 10:22:33 +0300

    updating more boilerplate code to be specific

--- src/option-page/App.js
+++ src/option-page/App.js
@@ -1,3 +1,8 @@
 export default function App() {
-  return <div>Options</div>;
+  return <div>
+    <h1>Thanks for using Twitch Assitant!</h1>
+    <p>This was created by Mcraimer, feel free to check out my stream: 
+      <a href="https://www.twitch.tv/mcraimer">www.twitch.tv/mcraimer</a> 
+    </p>
+  </div>;
 }
