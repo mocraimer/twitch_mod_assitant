40ea56e77c34443bcfa84d48dedc4081f8e7888b
mocraimer <good2k3@gmail.com>
2022-06-11 13:17:45 +0300

    popup can now display bots in chat

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import retrievalService from '../../../utils/retrivalService';
 
 const Popup = () => {
+
+  const [showBotList, setShowBotList] = useState(false);
+  const [botList, setBotList] = useState([]);
+  const [channelName, setChannelName] = useState('');
+
+
+  async function getBotsInChat() {
+    let bots = await retrievalService.getBotList();
+    let viewers = await retrievalService.getViewersInChat(await retrievalService.getChannelNameInCurrentTab());
+    let botNameKeyToAmountOfChannels = {};
+    bots.forEach(bot => {
+      botNameKeyToAmountOfChannels[bot[0]] = bot[1];
+    });
+    setShowBotList(false);
+    setBotList({});
+    let viewrBots = [];
+    viewers.forEach((user, index) => {
+      let amountOfChannels = botNameKeyToAmountOfChannels[user.toLowerCase()];
+      if (amountOfChannels) {
+        viewrBots.push({ name: user, amountOfChannels: amountOfChannels });
+      }
+
+    });
+    if (viewrBots.length > 0) {
+      setBotList(viewrBots);
+      setShowBotList(true);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <Button variant="primay" onClick={getBotsInChat}>Show Bots</Button>
+
+      <div className='bot-list'>
+        {showBotList && (() => `bots for ${channelName}`) &&
+          botList.map((bot, index) => {
+            return <div key={index}>{bot.name} is in {bot.amountOfChannels} channels</div>
+          })}
+      </div>
     </div>
   );
 };
