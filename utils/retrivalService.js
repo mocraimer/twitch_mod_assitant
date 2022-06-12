6d56dc4445d80510402b25b03ca986a29829b023
mocraimer <good2k3@gmail.com>
2022-06-12 08:01:56 +0300

    adding CORS anywhere for getting users in chat using api.allorigins.win

--- utils/retrivalService.js
+++ utils/retrivalService.js
@@ -26,26 +26,13 @@ class RetrievalService {
   }
 
   getViewersInChat = async (channelName) => {
-    let onlineChattersResponse = await fetch(`https://tmi.twitch.tv/group/user/${channelName}/chatters`,
-      {
-        "headers": {
-          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-          "accept-language": "en-US,en;q=0.9,he;q=0.8",
-          "cache-control": "max-age=0",
-          "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"102\", \"Google Chrome\";v=\"102\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"Windows\"",
-          "sec-fetch-dest": "document",
-          "sec-fetch-mode": "navigate",
-          "sec-fetch-site": "none",
-          "sec-fetch-user": "?1",
-          "upgrade-insecure-requests": "1"
-        },
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": null,
-        "method": "GET"
+    let acutalURL = encodeURIComponent(`https://tmi.twitch.tv/group/user/${channelName}/chatters`);
+    let onlineChattersResponse = await fetch(`https://api.allorigins.win/get?url=${acutalURL}`)
+      .then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
       });
-    let responseJson = await onlineChattersResponse.json();
+    let responseJson = JSON.parse(onlineChattersResponse.contents);
     return responseJson["chatters"]["viewers"];
   };
 
